c98b49e0b471f64932c79d06ab4c368b7c3859b0
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-03-19 14:32:48 -0300

    Adicionando pra valer um menu com animações, falta so a parte de rotas dele (19/03)

--- front-end/src/core/main/menu/menu.js
+++ front-end/src/core/main/menu/menu.js
@@ -1,7 +1,29 @@
 export default function startMenu() {
 
-    document.querySelector('i.menu-icon').addEventListener('click', () => {
-        console.log('menu')
+    const menu = {
+        open: document.querySelector('i.menu-icon.open'),
+        close: document.querySelector('i.menu-icon.close'),
+        container: document.querySelector('.menu-container'),
+        itself: document.querySelector('nav.menu')
+    }
+
+    menu.open.addEventListener('click', () => {
+        menu.container.classList.add('show-block')
+    })
+
+    menu.close.addEventListener('click', () => {
+        menu.itself.classList.add('hide-menu')
+    })
+
+    menu.container.addEventListener('click', () => {
+        menu.itself.classList.add('hide-menu')
+    })
+
+    menu.itself.addEventListener('animationend', e => {
+        if(e.animationName == 'hide-menu') {
+            menu.container.classList.remove('show-block')
+            menu.itself.classList.remove('hide-menu')
+        }
     })
 
 }
