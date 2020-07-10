81b71ff631912fed23ffc6e0c5ede35b6f5dac34
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-07-09 23:52:48 -0300

    Comment explaining the components object

--- front-end/src/core/main/componentsParser/componentsParser.js
+++ front-end/src/core/main/componentsParser/componentsParser.js
@@ -1,3 +1,24 @@
+/***
+  Current component attributes:
+  - html: name or path to component's html.
+  - css: name or path to component's css, if doesn't has css set this to "false" or "null".
+  - folder: folder name of current component, put this only if the attribute name is
+    diferent than the folder name.
+  - title: title who is going to be replaced when change routes.
+  - path: the path to the current component.
+  - init: a function who contains the logic of the component.
+  - name: default value of the title, html, css, folder and path attributes, use only if
+    one of them has the same or similar value, for example: {
+      html: 'comp.html',
+      css: 'comp.css,
+      folder: 'comp',
+      title: 'Comp',
+      path: '/comp'
+    },
+    the parser will make the needed changes on every attribute that is "undefined",
+    that is, attributes who is not setted.
+*/
+
 const parser = {
   title: parseTitle,
   html: name => `${name}.html`,
